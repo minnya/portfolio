a3978540f5503ebbe4ebc312487e49a15db42d8b
Hiro Miyoshi <38253944+minnya@users.noreply.github.com>
2026-03-05 23:06:54 -0500

    Delete CNAME

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,33 +1,30 @@
-import { createBrowserRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 import App from "../App";
 import { ShowcasePage } from "../pages/ShowcasePage";
 
-const router = createBrowserRouter(
-  [
-    {
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="about" replace />,
-        },
-        {
-          index: true,
-          path: "about",
-          element: <ShowcasePage />,
-        },
-        {
-          path: "showcase",
-          element: <ShowcasePage />,
-        },
-        {
-          path: "blog",
-          element: <ShowcasePage />,
-        },
-      ],
-    },
-  ],
-  { basename: "/portfolio" },
-);
+const router = createHashRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="about" replace />,
+      },
+      {
+        index: true,
+        path: "about",
+        element: <ShowcasePage />,
+      },
+      {
+        path: "showcase",
+        element: <ShowcasePage />,
+      },
+      {
+        path: "blog",
+        element: <ShowcasePage />,
+      },
+    ],
+  },
+]);
 
 export default router;
