00aa8e36bed63827d55cf9bc506d8f348566248e
hmiyoshi <hmiyoshi@ptservice.com>
2026-03-10 10:50:54 -0400

    Minimize chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          mui: ["@mui/material"],
+          muiIcons: ["@mui/icons-material"],
+          toolpad: ["@toolpad/core"],
+          reactSocialIcons: ["react-social-icons"],
+        },
+      },
+    },
   },
 });
